82e41f7962253842ce20618d2a08544e8b627046
J4m3s00 <james.lol2000@gmail.com>
2020-06-18 22:18:18 +0200

    Add glfw

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/104-Berlin/GoEngine
 
 go 1.14
+
+require github.com/go-gl/glfw v0.0.0-20200420212212-258d9bec320e
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
+func init() {
+	runtime.LockOSThread()
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, winErr := glfw.CreateWindow(1270, 720, "Hello World", nil, nil)
+	if winErr != nil {
+		panic(winErr)
+	}
+	window.MakeContextCurrent()
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
