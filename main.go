1cf35f79e2b7e180a7d5fa7c130b32a1d66b714d
J4m3s00 <james.lol2000@gmail.com>
2020-06-18 22:23:29 +0200

    Add gl

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"runtime"
 
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
@@ -22,7 +23,17 @@ func main() {
 		panic(winErr)
 	}
 	window.MakeContextCurrent()
+
+	//Init opengl
+	glErr := gl.Init()
+	if glErr != nil {
+		panic(glErr)
+	}
+
+	gl.ClearColor(1, 1, 1, 1)
 	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
